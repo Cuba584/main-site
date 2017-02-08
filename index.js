37174f049bddc406e555bc033e706201601e653a
kwboyd <kwboyd@live.unc.edu>
2017-02-07 22:54:08 -0500

    Implemented initial Google Docs json fetch.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var archieml = require('archieml');
+var parsed = archieml.load("key: value");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,5 +18,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
