ca13cde0690a3a425e5ede40e8847a57f0734270
lindsaycarbonell <lindsaycarbonell@gmail.com>
2017-02-09 15:15:09 -0500

    better linking

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
