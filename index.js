ccfee109d31d96396874a7f3b746733e991251f0
lindsaycarbonell <lindsaycarbonell@gmail.com>
2017-02-14 21:40:51 -0500

    consolidated routing to work with one inner page templatE

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var ejs = require('ejs');
 var archieml = require('archieml');
-var parsed = archieml.load("key: value");
+var parsed = archieml.load('key: value');
 var sent = require('./parse.js');
 var fs = require('fs');
-console.log(sent.sendIds());
+
+var pages = [
+  'aging', 'art', 'skate', 'tourism'
+]
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,57 +21,25 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.listen(app.get('port'), function() {
-  console.log(app.get('port'));
   console.log('Node app is running on port', app.get('port'));
 });
 
-
 app.get('/', function(request, response) {
-//  response.render('pages/index', {'body': thisText});
   response.render('pages/index');
-
 });
 
-app.get('/arttest', function(request, response) {
-//  response.render('pages/index', {'body': thisText});
- var file = JSON.parse(fs.readFileSync('./data/art.json'));
- console.log(file);
-  response.render('pages/arttest', {body: file});
-
-});
-
-app.get('/entretest', function(request, response) {
-//  response.render('pages/index', {'body': thisText});
-  if (typeof sent.entreBody !== 'undefined') {
-  response.render('pages/entretest', {body: sent.entreBody});
-} else {
+app.get('/pages/', function(request, response){
   response.render('pages/404');
-}
 });
 
-app.get('/tourismtest', function(request, response) {
-//  response.render('pages/index', {'body': thisText});
-  if (typeof sent.tourismBody !== 'undefined') {
-  response.render('pages/tourismtest', {body: sent.tourismBody});
-} else {
-  response.render('pages/404');
-}
+//gets the name of the page being requested and sends the relevant JSON file
+app.get('/pages/*', function(request, response){
+  var lastIdx = request.originalUrl.lastIndexOf('/');
+  var pageName = request.originalUrl.substring(lastIdx+1);
+  var bodyData = JSON.parse(fs.readFileSync('./data/' + pageName + '.json'));
+  response.render('pages/inner', {body: bodyData});
 });
 
-app.get('/skatetest', function(request, response) {
-//  response.render('pages/index', {'body': thisText});
-  if (typeof sent.skateBody !== 'undefined') {
-  response.render('pages/skatetest', {body: sent.skateBody});
-} else {
+app.get('*', function(request, response){
   response.render('pages/404');
-}
-});
-
-app.get('/agingtest', function(request, response) {
-//  response.render('pages/index', {'body': thisText});
-  if (typeof agingBody !== 'undefined') {
-  response.render('pages/agingtest', {body: sent.agingBody});
-} else {
-  response.render('pages/404');
-}
 });
