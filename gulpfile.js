d45a6abe46140e95aa7e728de76c13258ac4d2b5
lindsaycarbonell <lindsaycarbonell@gmail.com>
2017-02-25 11:37:05 -0500

    added gulp support

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var archieml = require('archieml');
+var parsed = archieml.load('key: value');
+var sent = require('./parse.js');
+var fs = require('fs');
+
+
+gulp.task('connect', function(){
+  var pages = [
+    'aging', 'art', 'skate', 'tourism', 'youth', 'entre'
+  ]
+
+  app.set('port', (process.env.PORT || 5000));
+  app.use(express.static(__dirname + '/public'));
+
+  // views is directory for all template files
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+
+  app.get('/', function(request, response) {
+    response.render('pages/index');
+  });
+
+  app.get('/pages/:id', function(req, res){
+    console.log(req.params.id);
+    var pageName = req.params.id;
+    var bodyData = JSON.parse(fs.readFileSync('./data/' + pageName + '.json'));
+    res.render('pages/inner', {body: bodyData});
+
+  });
+
+  app.get('*', function(request, response){
+    response.render('pages/404');
+  });
+
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+  console.log("compiling sass...");
+    return gulp.src('./sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/stylesheets/'));
+
+});
+
+gulp.task('watch', function() {
+    refresh.listen();
+    gulp.watch('./sass/*.scss', ['sass']);
+
+});
+
+
+gulp.task('default', ['connect', 'sass', 'watch']);
