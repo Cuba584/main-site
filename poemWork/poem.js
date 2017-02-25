210ac8557ce57dcb9b0648499d89b7e566827afd
peytonchance <peytonchance@gmail.com>
2017-02-25 14:14:47 -0500

    Worked on alternating backgrounds

--- poemWork/poem.js
+++ poemWork/poem.js
@@ -2,17 +2,17 @@ $(document).ready(function(){
 
 var sectionId = ["#section-0", "#section-0-img", "#section-1", "#section-1-img", "#section-2", "#section-2-img", "#section-3", "#section-3-img", "#section-4", "#section-4-img", "#section-5", "#section-5-img", "#section-6", "#section-6-img"];
 
-
+var rowId = ["#row-0", "#row-1", "#row-2", "#row-3", "#row-4", "#row-5", "#row-6"];
 
 
 for (var i in sectionId){
   (function(i){
-    var numCheck = sectionId[i].substring(9);
+    var numCheck = rowId[i].substring(5);
 
     $(sectionId[i]).css('opacity', 0);
 
     if((parseInt(numCheck)%2) != 0){
-      $(sectionId[i]).css('background-color', '#00C4C7');
+      $(rowId[i]).css('background-color', '#00C4C7');
       console.log(numCheck);
 
     }
