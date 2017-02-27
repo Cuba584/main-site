77ec79811542fc55e72e4f17be8f44fab97b4578
Alma Washington <alma19@live.unc.edu>
2017-02-27 10:59:32 -0500

    Homepage scroll.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,21 @@
-$(document).ready(function() {
-  $('#fullpage').fullpage({
-    verticalCentered: true,
-    sectionsColor: ['#ffffff', '#ffffff']
+/* Store the original positions */
+  var positions = [];
+  var cards = $('.card');
+  cards.each(function(){
+    //it's important to use offset because position is relative to the container, not the document
+    positions.push( $(this).offset().top );
   });
-});
+
+  /* respond to the scroll event */
+  $(window).on('scroll', function(e){
+      /* get the current scroll position */
+      var pos = $(window).scrollTop();
+
+      for (var i = 0; i < cards.length; i++) {
+        if( pos >= positions[i] ) {
+          $(cards[i]).addClass('latched');
+        } else {
+          $(cards[i]).removeClass('latched');
+        }
+      }
+  });
