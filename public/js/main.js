a440af103f25206de41f276fccdb43c473a5f2fc
Alma Washington <alma19@live.unc.edu>
2017-02-28 12:37:38 -0500

    Can fade and scroll now.

--- public/js/main.js
+++ public/js/main.js
@@ -57,29 +57,32 @@
 //
 // });
 
+$(document).ready(function(){
   AOS.init({
   duration: 1200,
   })
 
+  /* Store the original positions */
+    var positions = [];
+    var cards = $('.card');
+    cards.each(function(){
+      //it's important to use offset because position is relative to the container, not the document
+      positions.push( $(this).offset().top );
+    });
 
-/* Store the original positions */
-  var positions = [];
-  var cards = $('.card');
-  cards.each(function(){
-    //it's important to use offset because position is relative to the container, not the document
-    positions.push( $(this).offset().top );
-  });
+    /* respond to the scroll event */
+    $(window).on('scroll', function(e){
+        /* get the current scroll position */
+        var pos = $(window).scrollTop();
 
-  /* respond to the scroll event */
-  $(window).on('scroll', function(e){
-      /* get the current scroll position */
-      var pos = $(window).scrollTop();
-
-      for (var i = 0; i < cards.length; i++) {
-        if( pos >= positions[i] ) {
-          $(cards[i]).addClass('latched');
-        } else {
-          $(cards[i]).removeClass('latched');
+        for (var i = 0; i < cards.length; i++) {
+          if( pos >= positions[i] ) {
+            $(cards[i]).addClass('latched');
+          } else {
+            $(cards[i]).removeClass('latched');
+          }
         }
-      }
-  });
+    });
+
+
+});
