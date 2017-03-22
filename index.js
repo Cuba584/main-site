b7dcccd9d22317b4495f2d1ff59f35734af4c6dc
lindsaycarbonell <lindsaycarbonell@gmail.com>
2017-03-22 16:13:18 -0400

    fixing index.js to not be a duplicate of gulpfile.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+
 var express = require('express');
 var app = express();
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 var archieml = require('archieml');
 var parsed = archieml.load('key: value');
+var sent = require('./parse.js');
 var fs = require('fs');
 
-
-gulp.task('connect', function(){
-  var pages = [
-      'aging', 'art', 'skate', 'tourism', 'entre', 'artsespanol', 'agingespanol', 'youthespanol', 'harborespanol', 'entreespanol'
-    ]
+var pages = [
+    'aging', 'art', 'skate', 'tourism', 'entre', 'artsespanol', 'agingespanol', 'youthespanol', 'harborespanol', 'entreespanol'
+  ]
 
   app.set('port', (process.env.PORT || 5000));
   app.use(express.static(__dirname + '/public'));
@@ -23,6 +21,7 @@ gulp.task('connect', function(){
   app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
   });
+
 
   app.get('/', function(request, response) {
     response.render('pages/index', {page: 'index'});
@@ -51,21 +50,3 @@ gulp.task('connect', function(){
   app.get('*', function(request, response){
     response.render('pages/404', {page: '404'});
   });
-
-});
-
-gulp.task('sass', function() {
-  console.log("compiling sass...");
-    return gulp.src('./sass/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./public/stylesheets/'));
-
-});
-
-gulp.task('watch', function() {
-    gulp.watch('./sass/*.scss', ['sass']);
-
-});
-
-
-gulp.task('default', ['connect', 'sass', 'watch']);
