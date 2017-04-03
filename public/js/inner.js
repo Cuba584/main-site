a128634c1998ccb1db954a8a37b6e1df45a4d49c
kwboyd <kwboyd@live.unc.edu>
2017-04-03 01:31:43 -0400

    Fixed lights out resizing.

--- public/js/inner.js
+++ public/js/inner.js
@@ -1,27 +1,42 @@
 var videoTop;
 var videoHeight;
+var windowWidth;
 $(document).ready( function(){
-
+  windowWidth = $(window).width();
   videoTop = $('#video-top').offset().top;
   videoHeight = $('#vimeo-wrap').outerHeight();
-  $( window ).resize(function() {
-    windowWidth = $(window).width();
-    videoTop = $('#video-top').offset().top;
-    videoHeight = $('#vimeo-wrap').outerHeight();
-    $('#replay-btn').css('top', replayVideoHeight - (replayVideoHeight*0.35));
-  });
+  $('#replay-btn').css('top', replayVideoHeight - (replayVideoHeight*0.35));
   if (windowWidth > 1020) {
-  $(window).scroll(function(event){
+    goDark();
+  }
+});
+
+$( window ).resize(function() {
+  windowWidth = $(window).width();
+  videoTop = $('#video-top').offset().top;
+  videoHeight = $('#vimeo-wrap').outerHeight();
+  $('#replay-btn').css('top', replayVideoHeight - (replayVideoHeight*0.35));
+  if (windowWidth > 1020) {
+    goDark();
+  } else {
+    $('body').css('background-color', '#fff')
+
+  }
+});
+
+function goDark() {
     var scrollTop = $(this).scrollTop();
-
-
       if (scrollTop >= videoTop  - 200 && scrollTop <= videoTop + (videoHeight / 8)) {
         $('body').css('background-color', '#000')
       } else if (scrollTop > videoTop + (videoHeight / 8)) {
         $('body').css('background-color', '#fff')
       } else if (scrollTop < videoTop - 200) {
         $('body').css('background-color', '#fff')
-      }
-  });
-} else {};
+      };
+};
+
+$(window).scroll(function(event){
+  if (windowWidth > 1020) {
+    goDark();
+  }
 })
