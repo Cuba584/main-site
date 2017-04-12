09d54659dffb2f750446d639d66158f44612431d
Alma Washington <alma19@live.unc.edu>
2017-04-12 19:41:08 -0400

    Worked on mobile nav overlay.

--- public/js/main.js
+++ public/js/main.js
@@ -12,24 +12,25 @@ $(document).ready(function(){
     // $('#hidden-nav').show();
     document.getElementById("hidden-nav").style.width = "100%";
   });
+
   // menu animation
-  $('#nav-container').on('click', function(){
-    this.classList.toggle("change");
-    $('#hidden-nav').show();
-    $('#nav-container').hide();
+   $('#nav-container').on('click', function(){
+     $('#hidden-nav').show();
+     $('#nav-container').hide();
+   });
 
-  });
 
-  $('#hidden-nav').on('click', function (){
-    $('#nav-container').show();
-  });
+
+   $('#hidden-nav').on('click', function (){
+     $('#nav-container').show();
+   });
 
   $('#closebtn').click(function(){
 
-    document.getElementById("hidden-nav").style.width = "0%";
-    $('#nav-container')[0].classList.remove("change");
-    // $('#hidden-nav').hide();
-  });
+    // document.getElementById("hidden-nav").style.width = "0%";
+    //  $('#nav-container')[0].classList.remove("change");
+      $('#hidden-nav').hide();
+   });
 
 
   $('#trans-btn').on('click', function(){
