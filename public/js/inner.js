6995dfd457bbfb5e4cd4a259c8c9983feb24c70b
kwboyd <kwboyd@live.unc.edu>
2017-04-12 12:29:10 -0400

    Removed godark.

--- public/js/inner.js
+++ public/js/inner.js
@@ -1,43 +1,40 @@
-var videoTop;
-var videoHeight;
-var windowWidth;
-$(document).ready( function(){
-  windowWidth = $(window).width();
-  videoTop = $('#video-top').offset().top;
-  videoHeight = $('#vimeo-wrap').outerHeight();
-  if (windowWidth > 1020) {
-    goDark();
-  }
-});
-
-$( window ).resize(function() {
-  windowWidth = $(window).width();
-  videoTop = $('#video-top').offset().top;
-  videoHeight = $('#vimeo-wrap').outerHeight();
-  if (windowWidth > 1020) {
-    goDark();
-  } else {
-    $('body').css('background-color', '#fff')
-    $('.subhead').css('opacity', '1')
-  }
-});
-
-function goDark() {
-    var scrollTop = $(this).scrollTop();
-      if (scrollTop >= videoTop  - 200 && scrollTop <= videoTop + (videoHeight / 8)) {
-        $('body').css('background-color', '#000')
-        $('.subhead').css('opacity', '0')
-      } else if (scrollTop > videoTop + (videoHeight / 8)) {
-        $('body').css('background-color', '#fff')
-        $('.subhead').css('opacity', '1')
-      } else if (scrollTop < videoTop - 200) {
-        $('body').css('background-color', '#fff')
-        $('.subhead').css('opacity', '1')
-      };
-};
-
-$(window).scroll(function(event){
-  if (windowWidth > 1020) {
-    goDark();
-  }
-})
+// var videoTop;
+// var videoHeight;
+// var windowWidth;
+// $(document).ready( function(){
+//   windowWidth = $(window).width();
+//   videoTop = $('#video-top').offset().top;
+//   videoHeight = $('#vimeo-wrap').outerHeight();
+//   $(window).scroll(function(event){
+//     if (windowWidth > 1020) {
+//       goDark();
+//     }
+//   })
+// });
+// 
+// $( window ).resize(function() {
+//   windowWidth = $(window).width();
+//   videoTop = $('#video-top').offset().top;
+//   videoHeight = $('#vimeo-wrap').outerHeight();
+//   if (windowWidth > 1020) {
+//     goDark();
+//   } else {
+//     $('body').css('background-color', '#fff')
+//     $('p, h1, h2, h3, h4, h5, h6, #inner-title-box').css('opacity', '1')
+//   }
+// });
+//
+// function goDark() {
+//     var scrollTop = $(this).scrollTop();
+//     console.log(scrollTop)
+//       if (scrollTop >= videoTop - 200 && scrollTop <= videoTop + (videoHeight / 8)) {
+//         $('body').css('background-color', '#000')
+//         $('p, h1, h2, h3, h4, h5, h6, #inner-title-box').css('opacity', '0')
+//       } else if (scrollTop > videoTop + (videoHeight / 8)) {
+//         $('body').css('background-color', '#fff')
+//         $('p, h1, h2, h3, h4, h5, h6, #inner-title-box').css('opacity', '1')
+//       } else if (scrollTop < videoTop - 200) {
+//         $('body').css('background-color', '#fff')
+//         $('p, h1, h2, h3, h4, h5, h6, #inner-title-box').css('opacity', '1')
+//       };
+// };
