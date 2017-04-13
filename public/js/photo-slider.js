2ef169a059b90ab4d89d6c920e153f91f64f263b
kwboyd <kwboyd@live.unc.edu>
2017-04-13 02:48:27 -0400

    Fixed slider.

--- public/js/photo-slider.js
+++ public/js/photo-slider.js
@@ -13,58 +13,53 @@ var rightButton = document.getElementsByClassName('slider-right');
 var sliderLeft = sliderUl.offsetLeft;
 var slideIndex = 0;
 var captionIndex = 0;
-$('#inner-caption').html(captionArray[1]);
+$('#inner-caption').html(captionArray[0]);
+
+    $('.slider-left').on("click", function toPrevious() {
+      sliderUl.style.transition = "0.5s left";
+      slideIndex --;
+      console.log(slideIndex)
+      var captionIndex = slideIndex;
+      if (slideIndex == 0) {
+        sliderUl.style.left = "-100%";
+      } else if (slideIndex == -1) {
+        sliderUl.style.left = "0%";
+        setTimeout(function() {
+          sliderUl.style.transition = "0s left";
+          sliderUl.style.left = (sliderSections.length - 2) * -100 + '%';
+           slideIndex = sliderSections.length - 3;
+        }, 500);
+        captionIndex = sliderSections.length - 3;
+      }
+      else {
+        sliderUl.style.left = (slideIndex * -100) - 100 + '%';
+      }
+      var currentCaption = captionArray[captionIndex];
+      $('#inner-caption').html(currentCaption);
+    })
 
 
 
-  $('.slider-left').on("click", function toPrevious() {
-    sliderUl.style.transition = "0.5s left";
-    slideIndex --;
-    console.log(slideIndex)
-    var captionIndex = slideIndex;
-    if (slideIndex == 0) {
-      sliderUl.style.left = "-110%";
-    } else if (slideIndex == -1) {
-      sliderUl.style.left = "-10%";
-      setTimeout(function() {
-        sliderUl.style.transition = "0s left";
-        sliderUl.style.left = (sliderSections.length - 1) * -110 + '%';
-         slideIndex = sliderSections.length - 2;
-         console.log(slideIndex)
-      }, 500);
-    }
-    else {
-      sliderUl.style.left = (slideIndex * -110) - 110 + '%';
-    }
-    var currentCaption = captionArray[captionIndex +1];
-    $('#inner-caption').html(currentCaption);
-  })
-
-
-
-  $('.slider-right').on("click", function toNext() {
-    sliderUl.style.transition = "0.5s left";
-    slideIndex ++;
-    console.log(slideIndex)
-    captionIndex = slideIndex;
-    if (slideIndex == 0) {
-      sliderUl.style.left = "-110%";
-    }
-    else if (slideIndex > sliderSections.length - 2) {
-      sliderUl.style.left = (slideIndex * -110) - 110 + '%';
-      setTimeout(function() {
-        sliderUl.style.transition = "0s left";
-        sliderUl.style.left = "-110%";
-         slideIndex = 0;
-         console.log(slideIndex);
-      }, 500);
-      captionIndex = 0;
-    }
-    else {
-      sliderUl.style.left = (slideIndex * -110) - 110 + '%';
-    }
-    console.log(captionIndex + 1)
-    var currentCaption = captionArray[captionIndex +1];
-    console.log(currentCaption)
-    $('#inner-caption').html(currentCaption);
-  })
+    $('.slider-right').on("click", function toNext() {
+      sliderUl.style.transition = "0.5s left";
+      console.log(slideIndex);
+      slideIndex ++;
+      var captionIndex = slideIndex;
+      if (slideIndex == 0) {
+        sliderUl.style.left = "-100%";
+      }
+      else if (slideIndex > sliderSections.length - 3) {
+        sliderUl.style.left = (slideIndex * -100) - 100 + '%';
+        setTimeout(function() {
+          sliderUl.style.transition = "0s left";
+          sliderUl.style.left = "-100%";
+           slideIndex = 0;
+        }, 500);
+        captionIndex = 0;
+      }
+      else {
+        sliderUl.style.left = (slideIndex * -100) - 100 + '%';
+      }
+        var currentCaption = captionArray[captionIndex];
+        $('#inner-caption').html(currentCaption);
+    })
