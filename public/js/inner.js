a3533bb8bbfbd123f7eda617fcc0546b3bbaf4c3
Lindsay Marcella Carbonell <lcarbone@live.unc.edu>
2017-04-16 01:04:35 -0400

    all lights down

--- public/js/inner.js
+++ public/js/inner.js
@@ -1,40 +1,54 @@
-var videoTop;
-var videoHeight;
+var videoTop, videoHeight, videoTop2, videoHeight2;
 var windowWidth;
 $(document).ready( function(){
   windowWidth = $(window).width();
-  videoTop = $('#video-top').offset().top;
-  videoHeight = $('#vimeo-wrap').outerHeight();
+
+  updateVideoPositions();
+
   $(window).scroll(function(event){
-    if (windowWidth > 1280) {
-      goDark();
+    if (windowWidth > 1250) {
+      console.log(goDark(videoTop, videoHeight));
+      goDark(videoTop, videoHeight) || goDark(videoTop2, videoHeight2);
     }
   })
 });
 
+
 $( window ).resize(function() {
   windowWidth = $(window).width();
-  videoTop = $('#video-top').offset().top;
-  videoHeight = $('#vimeo-wrap').outerHeight();
-  if (windowWidth > 1280) {
-    goDark();
+  updateVideoPositions();
+
+  if (windowWidth > 1250) {
+    goDark(videoTop, videoHeight) || goDark(videoTop2, videoHeight2);
   } else {
     $('body').css('background-color', '#fff')
     $('p, h1, h2, h3, h4, h5, h6, #inner-title-box').css('opacity', '1')
   }
 });
 
-function goDark() {
+function goDark(top, height) {
     var scrollTop = $(this).scrollTop();
-    // console.log(scrollTop)
-      if (scrollTop >= videoTop - (videoHeight / 2) && scrollTop <= videoTop + (videoHeight / 10)) {
+  //   console.log(scrollTop)
+  //  console.log(top+", "+height);
+      if (scrollTop >= top - (height / 2) && scrollTop <= top + (height / 10)) {
         $('body').css('background-color', '#000')
         $('p, h1, h2, h3, h4, h5, h6, #inner-title-box').css('opacity', '0')
-      } else if (scrollTop > videoTop + (videoHeight / 10)) {
+        return true;
+      } else if (scrollTop > top + (height / 10)) {
         $('body').css('background-color', '#fff')
         $('p, h1, h2, h3, h4, h5, h6, #inner-title-box').css('opacity', '1')
-      } else if (scrollTop < videoTop - (videoHeight / 2)) {
+        return false;
+      } else if (scrollTop < top - (height / 2)) {
         $('body').css('background-color', '#fff')
         $('p, h1, h2, h3, h4, h5, h6, #inner-title-box').css('opacity', '1')
+        return false;
       };
+      return true;
+};
+
+var updateVideoPositions = function(){
+  videoTop = $('#video-top').offset().top;
+  videoHeight = $('#vimeo-wrap').outerHeight();
+  videoTop2 = $('#video-top2').offset().top;
+  videoHeight2 = $('#vimeo-wrap2').outerHeight();
 };
